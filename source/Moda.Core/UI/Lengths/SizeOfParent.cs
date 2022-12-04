22b3adf7b31a785e890580471786d4bce7cecd57
mattmag <matt@mattmag.dev>
2022-12-04 17:35:27 -0500

    Finish and test SizeOfParent

--- source/Moda.Core/UI/Lengths/SizeOfParent.cs
+++ source/Moda.Core/UI/Lengths/SizeOfParent.cs
@@ -12,27 +12,43 @@ namespace Moda.Core.UI.Lengths;
 
 public class SizeOfParent : Length
 {
+    private Option<Boundary> parentBoundary;
+    
+    public override Single Calculate()
+    {
+        return this.parentBoundary
+            .FlatMap(a => a.RelativeRange.Map(b => b.Delta))
+            .ValueOrFailure();
+    }
+    
+    
     protected override void OnInitialize(Cell owner, Axis axis)
     {
+        UpdateParentBoundary();
         ModifyPrerequisites(GetCoordinates(owner.Parent), Enumerable.Empty<Coordinate>());
+        
         owner.ParentChanged += OwnerOnParentChanged;
     }
 
-
+    
     private void OwnerOnParentChanged(Cell sender, ValueChangedArgs<Option<Cell>> args)
     {
+        UpdateParentBoundary();
         ModifyPrerequisites(GetCoordinates(args.NewValue), GetCoordinates(args.OldValue));
     }
 
 
+    private void UpdateParentBoundary()
+    {
+        this.parentBoundary = this.Axis.FlatMap(axis =>
+            this.Owner.FlatMap(owner =>
+                owner.Parent.Map(parent =>
+                    parent.GetBoundary(axis))));
+    }
+
+    
     private IEnumerable<Coordinate> GetCoordinates(Option<Cell> cell) => cell.Match(
         a => a.GetBoundary(this.Axis.ValueOrFailure()).GetCoordinates(),
         Enumerable.Empty<Coordinate>);
-
-
-    public override Single Calculate()
-    {
-        return 0.0f;
-    }
-
+    
 }
